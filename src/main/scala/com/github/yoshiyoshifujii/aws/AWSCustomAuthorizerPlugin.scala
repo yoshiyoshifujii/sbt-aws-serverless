4ccc46f4269efe9ef436fe21096d3f1b3a01a83f
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2016-09-14 19:34:30 +0900

    add AWSCustomAuthorizerPlugin

---
+++ src/main/scala/com/github/yoshiyoshifujii/aws/AWSCustomAuthorizerPlugin.scala
@@ -0,0 +1,58 @@
+package com.github.yoshiyoshifujii.aws
+
+import com.github.yoshiyoshifujii.aws.apigateway.{AWSApiGatewayAuthorize, AWSApiGatewayMethods, Uri}
+import com.github.yoshiyoshifujii.aws.lambda.AWSLambda
+import sbt._
+
+object AWSCustomAuthorizerPlugin extends AutoPlugin {
+
+  object autoImport {
+    lazy val awsAuthorizerName = settingKey[String]("")
+    lazy val awsIdentitySourceHeaderName = settingKey[String]("")
+    lazy val awsIdentityValidationExpression = settingKey[String]("")
+    lazy val awsAuthorizerResultTtlInSeconds = settingKey[Int]("")
+  }
+
+  import autoImport._
+  import AWSServerlessPlugin.autoImport._
+  import AWSApiGatewayPlugin.autoImport._
+
+  override lazy val projectSettings = Seq(
+    deployDev := {
+      val region = awsRegion.value
+      val lambdaName = awsLambdaFunctionName.value
+      val jar = sbtassembly.AssemblyKeys.assembly.value
+      val lambda = new AWSLambda(region)
+
+      lazy val deployLambda = lambda.deploy(
+        functionName = lambdaName,
+        role = awsLambdaRole.value,
+        handler = awsLambdaHandler.value,
+        bucketName = awsLambdaS3Bucket.value,
+        jar = jar,
+        description = awsLambdaDescription.?.value,
+        timeout = awsLambdaTimeout.?.value,
+        memorySize = awsLambdaMemorySize.?.value
+      )
+
+      (for {
+        lambdaArn <- deployLambda
+        _ = {println(s"Lambda Deploy: $lambdaArn")}
+        authorizerId <- new AWSApiGatewayAuthorize(region).deployAuthorizer(
+          restApiId = awsApiGatewayRestApiId.value,
+          name = awsAuthorizerName.value,
+          authorizerUri = Uri(
+            region,
+            awsAccountId.value,
+            lambdaName,
+            awsApiGatewayResourceUriLambdaAlias.?.value
+          ),
+          identitySourceHeaderName = awsIdentitySourceHeaderName.value,
+          identityValidationExpression = awsIdentityValidationExpression.?.value,
+          authorizerResultTtlInSeconds = awsAuthorizerResultTtlInSeconds.?.value
+        )
+        _ = {println(s"API Gateway Authorizer Deploy: $authorizerId")}
+      } yield jar).get
+    }
+  )
+}
--- src/main/scala/com/github/yoshiyoshifujii/aws/apigateway/AWSApiGateway.scala
+++ src/main/scala/com/github/yoshiyoshifujii/aws/apigateway/AWSApiGateway.scala
@@ -366,3 +366,111 @@ trait AWSApiGatewayMethodsWrapper extends AWSApiGatewayWrapper {
 }
 class AWSApiGatewayMethods(val regionName: String) extends AWSApiGatewayMethodsWrapper
 
+case class IdentitySource(header: String) {
+  lazy val mkValue = s"method.request.header.$header"
+}
+
+trait AWSApiGatewayAuthorizeWrapper extends AWSApiGatewayWrapper {
+
+  type AuthorizerId = String
+
+  def createAuthorizer(restApiId: RestApiId,
+                       name: String,
+                       authorizerUri: Uri,
+                       identitySourceHeaderName: String,
+                       identityValidationExpression: Option[String],
+                       authorizerResultTtlInSeconds: Option[Int] = Some(300)) = Try {
+    val request = new CreateAuthorizerRequest()
+      .withRestApiId(restApiId)
+      .withName(name)
+      .withType(AuthorizerType.TOKEN)
+      .withAuthType("custom")
+      .withAuthorizerUri(authorizerUri.value)
+      .withIdentitySource(IdentitySource(identitySourceHeaderName).mkValue)
+    identityValidationExpression.foreach(request.setIdentityValidationExpression)
+    authorizerResultTtlInSeconds.foreach(request.setAuthorizerResultTtlInSeconds(_))
+
+    client.createAuthorizer(request)
+  }
+
+  def getAuthorizers(restApiId: RestApiId) = Try {
+    val request = new GetAuthorizersRequest()
+      .withRestApiId(restApiId)
+
+    client.getAuthorizers(request)
+  }
+
+  def getAuthorizer(restApiId: RestApiId,
+                    name: String) =
+    for {
+      as <- getAuthorizers(restApiId)
+    } yield as.getItems.find(a => a.getName == name)
+
+  def updateAuthorizer(restApiId: RestApiId,
+                       authorizerId: AuthorizerId,
+                       name: String,
+                       authorizerUri: Uri,
+                       identitySourceHeaderName: String,
+                       identityValidationExpression: Option[String],
+                       authorizerResultTtlInSeconds: Option[Int] = Some(300)) = Try {
+    lazy val generatePatch =
+      (p: String) =>
+        (v: String) => Option {
+          new PatchOperation()
+            .withOp(Op.Replace)
+            .withPath(p)
+            .withValue(v)
+        }
+
+    lazy val patchOperations = Seq(
+      generatePatch("/name")(name),
+      generatePatch("/authorizerUri")(authorizerUri.value),
+      generatePatch("/identitySource")(IdentitySource(identitySourceHeaderName).mkValue),
+      identityValidationExpression.flatMap(generatePatch("/identityValidationExpression")(_)),
+      authorizerResultTtlInSeconds.flatMap(i => generatePatch("/authorizerResultTtlInSeconds")(i.toString))
+    ).flatten
+
+    val request = new UpdateAuthorizerRequest()
+      .withRestApiId(restApiId)
+      .withAuthorizerId(authorizerId)
+      .withPatchOperations(patchOperations.asJava)
+
+    client.updateAuthorizer(request)
+  }
+
+  def deployAuthorizer(restApiId: RestApiId,
+                       name: String,
+                       authorizerUri: Uri,
+                       identitySourceHeaderName: String,
+                       identityValidationExpression: Option[String],
+                       authorizerResultTtlInSeconds: Option[Int] = Some(300)) = {
+    for {
+      aOp <- getAuthorizer(restApiId, name)
+      id <- Try {
+        aOp map { a =>
+          updateAuthorizer(
+            restApiId = restApiId,
+            authorizerId = a.getId,
+            name = name,
+            authorizerUri = authorizerUri,
+            identitySourceHeaderName = identitySourceHeaderName,
+            identityValidationExpression = identityValidationExpression,
+            authorizerResultTtlInSeconds = authorizerResultTtlInSeconds
+          ).get.getId
+        } getOrElse {
+          createAuthorizer(
+            restApiId = restApiId,
+            name = name,
+            authorizerUri = authorizerUri,
+            identitySourceHeaderName = identitySourceHeaderName,
+            identityValidationExpression = identityValidationExpression,
+            authorizerResultTtlInSeconds = authorizerResultTtlInSeconds
+          ).get.getId
+        }
+      }
+    } yield id
+  }
+
+}
+class AWSApiGatewayAuthorize(val regionName: String) extends AWSApiGatewayAuthorizeWrapper
+
